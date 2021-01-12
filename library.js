a1748094b1af9b80c1b7cdb029201b76dc0c9239
Antosik <grig_anton96@mail.ru>
2021-01-12 15:59:48 +0300

    style: codestyle

--- library.js
+++ library.js
@@ -9,17 +9,16 @@ const plugin = {};
 plugin.init = function (params, callback) {
 	const router = params.router;
 	const hostMiddleware = params.middleware;
-	// const hostControllers = params.controllers;
 
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
-
 	router.get('/admin/plugins/category-queue', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
 	router.get('/api/admin/plugins/category-queue', controllers.renderAdminPage);
-	meta.settings.get('category-queue', function(err, settings) {
+
+	meta.settings.get('category-queue', function (err, settings) {
 		if (err) {
 			winston.error('[plugin/category-queue] Could not retrieve plugin settings!');
-			plugin.settings = {"1":""};
+			plugin.settings = {};
 			return;
 		}
 
@@ -44,11 +43,10 @@ plugin.postQueue = function (postData, callback) {
 		if (Object.values(plugin.settings).includes(String(postData.data.cid))) {
 			postData.shouldQueue = true;
 		}
+		callback(null, postData);
 	} catch (error) {
-		
+		callback(error);
 	}
-	
-	callback(null, postData);
 };
 
 module.exports = plugin;
