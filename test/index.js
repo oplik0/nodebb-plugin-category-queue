042c18d177101e2cad1bc71c881763c27fb02601
Opliko <opliko.reg@protonmail.com>
2023-11-03 23:30:39 +0100

    style: run dprint

--- test/index.js
+++ test/index.js
@@ -20,7 +20,6 @@
 
 const assert = require('assert');
 
-
 const topics = require.main.require('./src/topics');
 const posts = require.main.require('./src/posts');
 const categories = require.main.require('./src/categories');
@@ -52,43 +51,93 @@ describe('nodebb-plugin-category-queue', () => {
 		initialExemptGroups = meta.config.groupsExemptFromPostQueue;
 		await meta.configs.set('groupsExemptFromPostQueue', [...initialExemptGroups, group.name]);
 		await groups.join('administrators', adminUid);
-		tid = await topics.create({ title: 'Test Topic', content: 'Test topic created by testing script', uid: adminUid, cid });
+		tid = await topics.create({
+			title: 'Test Topic',
+			content: 'Test topic created by testing script',
+			uid: adminUid,
+			cid,
+		});
 	});
 
 	it('should not queue any new topics by default', async () => {
-		assert(!await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }), "Unprivileged user's topic was queued");
-		assert(!await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }), "Exempt user's topic was queued");
-		assert(!await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }), "Administrator's topic was queued");
+		assert(
+			!await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }),
+			"Unprivileged user's topic was queued",
+		);
+		assert(
+			!await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }),
+			"Exempt user's topic was queued",
+		);
+		assert(
+			!await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }),
+			"Administrator's topic was queued",
+		);
 	});
 
 	it('should not queue any new replies by default', async () => {
-		assert(!await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }), "Unprivileged user's reply was queued");
-		assert(!await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }), "Exempt user's reply was queued");
-		assert(!await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }), "Administrator's reply was queued");
+		assert(
+			!await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }),
+			"Unprivileged user's reply was queued",
+		);
+		assert(
+			!await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }),
+			"Exempt user's reply was queued",
+		);
+		assert(
+			!await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }),
+			"Administrator's reply was queued",
+		);
 	});
 
 	it('should queue new topics by all users if enabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
-		assert(await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }), "Unprivileged user's topic was not queued");
-		assert(await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }), "Exempt user's topic was not queued");
-		assert(await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }), "Administrator's topic was not queued");
+		assert(
+			await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }),
+			"Unprivileged user's topic was not queued",
+		);
+		assert(
+			await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }),
+			"Exempt user's topic was not queued",
+		);
+		assert(
+			await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }),
+			"Administrator's topic was not queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
 
 	it('should queue new replies by all users if enabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
-		assert(await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }), "Unprivileged user's reply was not queued");
-		assert(await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }), "Exempt user's reply was not queued");
-		assert(await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }), "Administrator's reply was not queued");
+		assert(
+			await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }),
+			"Unprivileged user's reply was not queued",
+		);
+		assert(
+			await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }),
+			"Exempt user's reply was not queued",
+		);
+		assert(
+			await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }),
+			"Administrator's reply was not queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
 
 	it('should not queue new topics by privileged users if enabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
 		await meta.settings.setOne('category-queue', `${cid}-privileged`, 'on');
-		assert(await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }), "Unprivileged user's topic was not queued");
-		assert(await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }), "Exempt user's topic was not queued");
-		assert(!await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }), "Administrator's topic was queued");
+		assert(
+			await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }),
+			"Unprivileged user's topic was not queued",
+		);
+		assert(
+			await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }),
+			"Exempt user's topic was not queued",
+		);
+		assert(
+			!await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }),
+			"Administrator's topic was queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-privileged`, 'off');
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
@@ -96,9 +145,18 @@ describe('nodebb-plugin-category-queue', () => {
 	it('should not queue new replies by privileged users if enabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
 		await meta.settings.setOne('category-queue', `${cid}-privileged`, 'on');
-		assert(await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }), "Unprivileged user's reply was not queued");
-		assert(await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }), "Exempt user's reply was not queued");
-		assert(!await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }), "Administrator's reply was queued");
+		assert(
+			await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }),
+			"Unprivileged user's reply was not queued",
+		);
+		assert(
+			await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }),
+			"Exempt user's reply was not queued",
+		);
+		assert(
+			!await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }),
+			"Administrator's reply was queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-privileged`, 'off');
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
@@ -106,9 +164,18 @@ describe('nodebb-plugin-category-queue', () => {
 	it('should not queue new topics by exempt groups if enabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
 		await meta.settings.setOne('category-queue', `${cid}-exempt`, 'on');
-		assert(await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }), "Unprivileged user's topic was not queued");
-		assert(!await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }), "Exempt user's topic was queued");
-		assert(await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }), "Administrator's topic was not queued");
+		assert(
+			await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }),
+			"Unprivileged user's topic was not queued",
+		);
+		assert(
+			!await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }),
+			"Exempt user's topic was queued",
+		);
+		assert(
+			await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }),
+			"Administrator's topic was not queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-exempt`, 'off');
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
@@ -116,9 +183,18 @@ describe('nodebb-plugin-category-queue', () => {
 	it('should not queue new replies by exempt groups if enabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
 		await meta.settings.setOne('category-queue', `${cid}-exempt`, 'on');
-		assert(await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }), "Unprivileged user's reply was not queued");
-		assert(!await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }), "Exempt user's reply was queued");
-		assert(await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }), "Administrator's reply was not queued");
+		assert(
+			await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }),
+			"Unprivileged user's reply was not queued",
+		);
+		assert(
+			!await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }),
+			"Exempt user's reply was queued",
+		);
+		assert(
+			await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }),
+			"Administrator's reply was not queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-exempt`, 'off');
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
@@ -126,9 +202,18 @@ describe('nodebb-plugin-category-queue', () => {
 	it('should not queue replies if disabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
 		await meta.settings.setOne('category-queue', `${cid}-no-replies`, 'on');
-		assert(!await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }), "Unprivileged user's reply was queued");
-		assert(!await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }), "Exempt user's reply was queued");
-		assert(!await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }), "Administrator's reply was queued");
+		assert(
+			!await posts.shouldQueue(unprivilegedUid, { tid, content: 'Just a test reply' }),
+			"Unprivileged user's reply was queued",
+		);
+		assert(
+			!await posts.shouldQueue(exemptUid, { tid, content: 'Just a test reply' }),
+			"Exempt user's reply was queued",
+		);
+		assert(
+			!await posts.shouldQueue(adminUid, { tid, content: 'Just a test reply' }),
+			"Administrator's reply was queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-no-replies`, 'off');
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
@@ -136,9 +221,18 @@ describe('nodebb-plugin-category-queue', () => {
 	it('should still queue topics if replies are disabled', async () => {
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'on');
 		await meta.settings.setOne('category-queue', `${cid}-no-replies`, 'on');
-		assert(await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }), "Unprivileged user's topic was not queued");
-		assert(await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }), "Exempt user's topic was not queued");
-		assert(await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }), "Administrator's topic was not queued");
+		assert(
+			await posts.shouldQueue(unprivilegedUid, { cid, content: 'Just a test topic' }),
+			"Unprivileged user's topic was not queued",
+		);
+		assert(
+			await posts.shouldQueue(exemptUid, { cid, content: 'Just a test topic' }),
+			"Exempt user's topic was not queued",
+		);
+		assert(
+			await posts.shouldQueue(adminUid, { cid, content: 'Just a test topic' }),
+			"Administrator's topic was not queued",
+		);
 		await meta.settings.setOne('category-queue', `${cid}-no-replies`, 'off');
 		await meta.settings.setOne('category-queue', `${cid}-enabled`, 'off');
 	});
